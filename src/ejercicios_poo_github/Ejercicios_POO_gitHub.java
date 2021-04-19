d5ba15c67c9c6020d3ec0777ebbc399c5d53e70a
Yoselin <Yoselin@DESKTOP-6T1QARU>
2021-04-19 15:50:23 -0600

    Programa que despliega el nombre de la alumna

---
+++ src/ejercicios_poo_github/Ejercicios_POO_gitHub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios_poo_github;
+
+/**
+ *
+ * @author Yoselin
+ */
+public class Ejercicios_POO_gitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy Yoselin Licón :D ");
+    }
+    
+}
